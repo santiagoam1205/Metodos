719de2741e2bd2110d38fe7afaebac6d917c69c4
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-08-25 13:28:18 -0500

    <Fix> ejercicio3: Correccion en cantidad de codigos innecesarios, como guaradr los datos en String para luego convertirlos

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class Ejercicio3 {
+    public static double calculararea(double base, double altura){
+        return base*altura;
+    }
+
+    static void main(String[] args) {
+        double base= Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base: "));
+
+        double altura= Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura: "));
+
+        double area= calculararea(base,altura);
+
+        JOptionPane.showMessageDialog(null,"El area del base es: "+area);
+    }
 }
